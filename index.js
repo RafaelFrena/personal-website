5250100981cb561230d8ae6dd7e1e1a03e6db904
RafaelFrena <rfaelfrena@gmail.com>
2022-09-14 22:20:52 -0300

    ...

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ addEventListener('scroll', (event) => {
     // toggle about button boldness
     if (html.scrollTop > 676 && html.scrollTop < 1428) {
         aboutButton.style = "font-weight: bold;"
+        console.log("window.location.href: ", window.location.href);
+        if (!window.location.href.includes("#about")) {
+            window.location.replace("#about");
+        }
     } else {
         aboutButton.style = "font-weight: initial;"
     }
@@ -22,6 +26,9 @@ addEventListener('scroll', (event) => {
     // toggle portfolio button boldness
     if (html.scrollTop > 1428 && html.scrollTop < 2160) {
         portfolioButton.style = "font-weight: bold;"
+        if (!window.location.href.includes("#portfolio")) {
+            window.location.replace("#portfolio");
+        }
     } else {
         portfolioButton.style = "font-weight: initial;"
     }
@@ -29,6 +36,9 @@ addEventListener('scroll', (event) => {
     // toggle contact button boldness
     if (html.scrollTop > 2160) {
         contactButton.style = "font-weight: bold;"
+        if (!window.location.href.includes("#contact")) {
+            window.location.replace("#contact");
+        }
     } else {
         contactButton.style = "font-weight: initial;"
     }
